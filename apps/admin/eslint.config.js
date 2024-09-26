190848c44cedbe4c16aafaba44bab99e5fc32cad
Brendan Bx <Brendanbx@gmail.com>
2024-09-26 16:28:28 +0200

    Added Admin Dependencies

--- apps/admin/eslint.config.js
+++ apps/admin/eslint.config.js
@@ -1,13 +1,22 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
-import pluginReact from "eslint-plugin-react";
+// @ts-check
 
+import globals from "globals"
+import pluginJs from "@eslint/js"
+import tseslint from "typescript-eslint"
+import pluginReact from "eslint-plugin-react"
+import pluginQuery from "@tanstack/eslint-plugin-query"
 
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"]},
-  {languageOptions: { globals: globals.browser }},
-  pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
-  pluginReact.configs.flat.recommended,
-];
+	{ files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
+	{ languageOptions: { globals: globals.browser } },
+	pluginJs.configs.recommended,
+	...tseslint.configs.recommended,
+	pluginReact.configs.flat.recommended,
+	...pluginQuery.configs["flat/recommended"],
+	{
+		rules: {
+			"react/react-in-jsx-scope": 0,
+			"react/jsx-uses-react": 0,
+		},
+	},
+]

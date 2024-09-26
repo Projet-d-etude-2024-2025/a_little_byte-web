b1f3678592c6d88afade5fd6d3b5cbce41b658fe
Brendan Bx <Brendanbx@gmail.com>
2024-09-26 15:24:21 +0200

    Adding components shadcn-ui

--- apps/admin/src/main.tsx
+++ apps/admin/src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
 import App from "./App.tsx"
+import "@alittlebyte/components/shadcn-ui.css"
 
 createRoot(document.getElementById("root")!).render(
 	<StrictMode>

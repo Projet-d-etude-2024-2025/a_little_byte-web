0e1b6f455605fc7f6f45f0f226c9ede801e2b52b
Vincent R <44976231+Limerio@users.noreply.github.com>
2024-09-27 11:22:46 +0200

    TAS-54/multi-lang (#10)
    * fix: tanstack query add context and provider
    
    * tech: init i18next

--- apps/landing/src/routes/index.lazy.tsx
+++ apps/landing/src/routes/index.lazy.tsx
@@ -1,13 +1,16 @@
 import { createLazyFileRoute } from "@tanstack/react-router"
+import { useTranslation } from "react-i18next"
 
 export const Route = createLazyFileRoute("/")({
 	component: Index,
 })
 
 function Index() {
+	const [t] = useTranslation("common")
+
 	return (
 		<div className="p-2">
-			<h3>Welcome Home!</h3>
+			<h3>{t("common:welcome")}</h3>
 		</div>
 	)
 }

6f28c61bb527898d85cf4593aa55a6bb09924d60
Vincent Raoul <vincent.raoul@cyna-it.fr>
2024-09-27 10:55:53 +0200

    fix: tanstack query add context and provider

--- apps/landing/src/main.tsx
+++ apps/landing/src/main.tsx
@@ -1,27 +1,27 @@
+import { routeTree } from "@alittlebyte/landing/routeTree.gen"
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 import { RouterProvider, createRouter } from "@tanstack/react-router"
 import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
 
-// Import the generated route tree
-import { routeTree } from "./routeTree.gen"
+const queryClient = new QueryClient()
 
-// Create a new router instance
-const router = createRouter({ routeTree })
+const router = createRouter({ routeTree, context: { queryClient } })
 
-// Register the router instance for type safety
 declare module "@tanstack/react-router" {
 	interface Register {
 		router: typeof router
 	}
 }
 
-// Render the app
 const rootElement = document.getElementById("root")!
 if (!rootElement.innerHTML) {
 	const root = createRoot(rootElement)
 	root.render(
 		<StrictMode>
-			<RouterProvider router={router} />
+			<QueryClientProvider client={queryClient}>
+				<RouterProvider router={router} />
+			</QueryClientProvider>
 		</StrictMode>,
 	)
 }
--- apps/landing/src/routes/__root.tsx
+++ apps/landing/src/routes/__root.tsx
@@ -1,22 +1,26 @@
-import { createRootRoute, Link, Outlet } from "@tanstack/react-router"
-import { TanStackRouterDevtools } from "@tanstack/router-devtools"
+import { QueryClient } from "@tanstack/react-query"
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools"
+import {
+	createRootRouteWithContext,
+	Link,
+	Outlet,
+} from "@tanstack/react-router"
+import { TanStackRouterDevtools } from "@tanstack/router-devtools"
 
-export const Route = createRootRoute({
-	component: () => (
-		<>
-			<div className="flex gap-2 p-2">
-				<Link to="/" className="[&.active]:font-bold">
-					Home
-				</Link>{" "}
-				<Link to="/about" className="[&.active]:font-bold">
-					About
-				</Link>
-			</div>
-			<hr />
-			<Outlet />
-			<ReactQueryDevtools buttonPosition="top-right" />
-			<TanStackRouterDevtools position="bottom-right" />
-		</>
-	),
-})
+export const Route = createRootRouteWithContext<{ queryClient: QueryClient }>()(
+	{
+		component: () => (
+			<>
+				<div className="flex gap-2 p-2">
+					<Link to="/" className="[&.active]:font-bold">
+						Home
+					</Link>
+				</div>
+				<hr />
+				<Outlet />
+				<ReactQueryDevtools buttonPosition="top-right" />
+				<TanStackRouterDevtools position="bottom-right" />
+			</>
+		),
+	},
+)

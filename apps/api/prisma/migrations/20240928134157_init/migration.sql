8e60e8b6230bc444682f07ae3f668d34b7100c95
Vincent R <44976231+Limerio@users.noreply.github.com>
2024-09-28 16:02:36 +0200

    TAS-80/update-users-model (#13)
    * chore: move the prisma lib into the source file
    
    * chore: update the prisma schema for the user model
    
    * chore: create the first migration
    
    * chore: add script in api package.json and the root package.json
    
    * fix: eslint rule
    
    * chore: add the prisma injection into the context

---
+++ apps/api/prisma/migrations/20240928134157_init/migration.sql
@@ -0,0 +1,18 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "firstName" TEXT NOT NULL,
+    "lastName" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "passwordHash" TEXT NOT NULL,
+    "passwordSalt" TEXT NOT NULL,
+    "verifiedToken" TEXT,
+    "emailVerifiedAt" TIMESTAMP(3),
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_firstName_lastName_email_key" ON "User"("firstName", "lastName", "email");

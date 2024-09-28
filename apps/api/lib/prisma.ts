f66ab65fbf0f905a7063d9758d9cd19c4d921553
WookiesRpeople2 <keentobiansky@gmail.com>
2024-09-28 11:41:17 +0200

    Fixed: formatting error on api

--- apps/api/lib/prisma.ts
+++ apps/api/lib/prisma.ts
@@ -3,13 +3,13 @@
 import { PrismaClient } from "@prisma/client"
 
 declare global {
-  var prisma: PrismaClient | undefined
+	var prisma: PrismaClient | undefined
 }
 
 const prismaDb = globalThis.prisma || new PrismaClient()
 
 if (process.env.NODE_ENV !== "production") {
-  globalThis.prisma = prismaDb
+	globalThis.prisma = prismaDb
 }
 
 export default prismaDb
